44d4fa3c54da253dd863ab8096f58a97cdfb1f22
kahnu044 <kahnucharan044@gmail.com>
2024-11-21 16:18:26 +0530

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React, {useEffect, useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Home = () => {
+  return (
+    <View style={{flex:1,justifyContent: "center", alignItems: "center"}}>
+      <Text>Hi,Welcome To Pusher App</Text>
+    </View>
+  );
+};
+
+export default Home;
